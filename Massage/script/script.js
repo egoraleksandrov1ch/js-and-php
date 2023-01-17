2332baf778484d2ca7d9ec6a18d6b0bdd54ea41b
Egor Aleksandrovich <egor09662@gmail.com>
2023-01-17 20:49:20 +0100

    add new translations

--- Massage/script/script.js
+++ Massage/script/script.js
@@ -3,7 +3,23 @@ let multiLanguage = {
         header: ["На главную", "Частые вопросы", "Контакты"],
         main: {
             title: ["Классический массаж тела", "Релакс массаж", "Антицеллюлитный массаж", "Лимфодренажный массаж", "Спортивный массаж"],
-            description: [[], [], [], [], []],
+            description: [
+                [
+                    "Классический массаж прорабатывает все основные зоны<br><br>Это одна из отличительных особенностей. Специалист в процессе работы прорабатывает голову, спину, живот, руки и ноги. Оказывает целостное воздействие на организм. Какие результаты даёт курс классического массажа:<br><br>1. В процессе проработки головы и шеи улучшается кровообращение, пропадают боли и напряжение, ускоряется рост волос. Всего пару процедур способны избавить от нервного напряжения и снизить внутриглазное давление.<br>2. Массаж живота налаживает работу желудочно-кишечного тракта, способствует похудению. Также нельзя забывать о том, что такая процедура служит профилактикой появления целлюлита.<br>3. Классический массаж спины укрепляет мышечный корсет, выравнивает осанку, снимает напряжение и ощущение тяжёлых плеч, а также избавляет от спазмов и защемлений. Это особенно актуально для тех, кто ведёт малоактивный образ жизни или напротив, подвержен серьёзным физическим нагрузкам.<br>4. Массаж ног улучшает кровообращение и лимфоток, предотвращает варикозное расширение вен. А ещё отлично снимает чувство усталости, после сеанса пациент чувствует себя обновлённым."
+                ], 
+                [
+                    "Расслабляющий релакс массаж – очень приятная процедура, которая позволяет добиться полного восстановления и расслабления. Она наполняет новыми силами и свежими идеями, дарит ощущение полета и радости жизни.<br><br>Релакс массаж всего тела заставляет ваши мышцы расслабиться и восстановить во всем теле жизненные силы.<br><br>Эта процедура будет полезна как в качестве лечебного, так и профилактического метода. Для здоровых людей расслабляющий массаж станет отличной анти-стрессовой процедурой, она поможет снять напряжение после трудовых дней.<br><br>Механическое воздействие на периферические нервные окончания стимулируют нервную систему, заставляя организм вырабатывать эндорфины - гормоны счастья."
+                ], 
+                [
+                    "Вы давно мечтали об идеальной гладкой коже без «апельсиновой корки»?<br><br>Для начала отметим, что антицеллюлитный массаж способен улучшить общее состояние кожи, а также благотворно сказывается на состояние подкожно-жирового слоя. После ряда процедур происходит активизация обмена веществ, усиливается кровообращение. В конечном итоге это способствует более быстрому похудению.<br><br>Антицеллюлитный массаж применяется на таких зонах: бедра, ягодицы, живот. После проведения моделирующего массажа можно добиться таких результатов:<br><br>1.  увеличение лимфотока и кровообращения;<br>2. нормализация обмена веществ;<br>3. снижение проявлений целлюлита;<br>4. коррекция фигуры;<br>5. увеличение эластичности, упругости и цвета кожи."
+                ], 
+                [
+                    "Лимфодренажный массаж – относительно безболезненный процесс, подразумевающий поглаживания и надавливания. Все манипуляции деликатные и волнообразные, выполняются медленно и весьма мягко. Интенсивность воздействия увеличивается к середине сеанса.<br><br>Она активирует кровообращение, способствует циркуляции лимфы, улучшает обмен веществ, сжигает жиры. Всё это не только помогает похудению, но и в целом хорошо влияет на организм."
+                ], 
+                [
+                    "Каждому хочется иметь подтянутое тело. Чтобы достичь желаемого многие отправляются в тренажерные залы, корректируют свой рацион. При таком режиме полезен спортивный массаж.<br><br>Несильные массирующие движения расслабляют и улучшают работу внутренних систем. Процедуру делают после тренировки или перед нагрузками. В первом случае она нужна для регенерации сил спортсмена. Во втором – играет роль разминки.<br><br>Массирование усиливает кровоток, регулирует функцию сальных желез. В процессе оптимизируется работа сосудов кожи, они укрепляются, повышается их тонус. Помимо этого, массаж помогает снять усталость мышц, разработать суставы."
+                ]
+            ],
             fon: ["./media/classic.jpeg", "./media/relax.jpeg", "./media/anticelulit.jpeg", "./media/limf.jpeg", "./media/sport.jpeg"]
         },
         form: {
@@ -16,7 +32,23 @@ let multiLanguage = {
         header: ["Home", "FAQ", "Contact"],
         main: {
             title: ["Classical body massage", "Relax Massage", "Anti-cellulite massage", "Lymphatic drainage massage", "Sports massage"],
-            description: [[], [], [], [], []],
+            description: [
+                [
+                    "Classic massage works on all major areas<br><br>This is one of the distinctive features. The specialist works on the head, back, abdomen, arms and legs. It has a holistic effect on the body. What results a course of classical massage gives:<br><br>1. In the process of working on the head and neck, blood circulation improves, pains and tension disappear, and hair growth is accelerated. Just a couple of procedures can relieve nervous tension and reduce intraocular pressure.<br>2. The abdominal massage improves the function of the gastrointestinal tract and promotes weight loss. Also, we should not forget that this procedure serves as a prevention of the appearance of cellulite.<br>3. Classic back massage strengthens the muscular corset, aligns the posture, relieves tension and the feeling of heavy shoulders, and relieves spasms and stiffness. This is especially true for those who lead an inactive lifestyle or, on the contrary, are subjected to serious physical exertion.<br>4. Foot massage improves blood circulation and lymph flow, prevents varicose veins. It is also excellent for relieving fatigue and makes the patient feel refreshed after the session."
+                ], 
+                [
+                    "Relaxing relaxation massage is a very pleasant procedure that allows you to achieve complete recovery and relaxation. It fills you with new strength and fresh ideas and gives you a feeling of flight and joy of life.<br><br>A full-body relaxation massage causes your muscles to relax and restore vitality throughout your body.<br><br>This procedure will be useful both as a therapeutic and preventive method. For healthy people relaxing massage will be an excellent anti-stress procedure, it will help relieve stress after days of work.<br><br>The mechanical action on the peripheral nerve endings stimulates the nervous system, causing the body to produce endorphins, the hormones of happiness."
+                ], 
+                [
+                    "Have you long dreamed of perfect smooth skin without 'orange peel'?<br><br>To begin with, let us note that anti-cellulite massage can improve the overall condition of the skin, as well as having a beneficial effect on the condition of the subcutaneous fat layer. After a number of procedures there is an activation of metabolism, increased blood circulation. Ultimately, this contributes to a more rapid weight loss.<br><br>Anti-cellulite massage is used on the following areas: hips, buttocks, abdomen. After a modeling massage you can achieve these results:<br><br>1. increase in lymph flow and blood circulation;<br>2. normalization of metabolism;<br>3. reducing the appearance of cellulite;<br>4. figure correction;<br>5. increase skin elasticity, firmness and color."
+                ], 
+                [
+                    "Lymphatic drainage massage is a relatively painless process that involves stroking and pressing. All manipulations are delicate and undulating, performed slowly and very gently. The intensity increases towards the middle of the session.<br><br>It activates blood circulation, promotes lymph circulation, improves metabolism, and burns fat. All this not only helps weight loss, but also has a generally good effect on the body."
+                ], 
+                [
+                    "Everyone wants to have a toned body. To achieve the desired body, many people go to gyms, adjust their diet. A sports massage is useful in this regimen.<br><br>Light massaging movements relax and improve the function of internal systems. The procedure is done after a workout or before an exertion. In the first case, it is needed to regenerate the athlete's strength. In the second case, it plays the role of a warm-up.<br><br>Massaging increases blood flow, regulates the function of the sebaceous glands. In the process, skin vessels are optimized, they are strengthened, and their tone is increased. In addition, the massage helps to relieve muscle fatigue and develop the joints."
+                ]
+            ],
             fon: ["./media/classic.jpeg", "./media/relax.jpeg", "./media/anticelulit.jpeg", "./media/limf.jpeg", "./media/sport.jpeg"]
         },
         form: {
@@ -29,7 +61,23 @@ let multiLanguage = {
         header: ["Strona główna", "Częste pytania", "Kontakt"],
         main: {
             title: ["Masaż klasyczny ciała", "Masaż relaksacyjny", "Masaż antycellulitowy", "Masaż drenażowy limfatyczny", "Masaż sportowy"],
-            description: [[], [], [], [], []],
+            description: [
+                [
+                    "Masaż klasyczny działa na wszystkie główne obszary<br><br>Jest to jedna z cech wyróżniających. Praktyk pracuje na głowie, plecach, brzuchu, rękach i nogach. Działa holistycznie na organizm. Jakie są efekty kursu masażu klasycznego:<br><br>1. Zabieg na głowę i szyję poprawia krążenie, łagodzi ból i napięcie oraz przyspiesza wzrost włosów. Już kilka zabiegów może złagodzić napięcie nerwowe i obniżyć ciśnienie wewnątrzgałkowe.<br>2. Masaż brzucha wspomaga funkcjonowanie układu pokarmowego i sprzyja odchudzaniu. Pomaga również zapobiegać pojawianiu się cellulitu.<br>3. Masaż klasyczny pleców wzmacnia gorset mięśniowy, wyrównuje postawę, łagodzi napięcia i uczucie ciężkości ramion, łagodzi skurcze i sztywność. Jest to szczególnie istotne dla osób prowadzących mało aktywny tryb życia lub skłonnych do poważnego wysiłku fizycznego.<br>4. Masaż stóp poprawia krążenie krwi i limfy oraz zapobiega powstawaniu żylaków. Doskonale sprawdza się również w łagodzeniu zmęczenia i sprawia, że po sesji pacjent czuje się odświeżony."
+                ], 
+                [
+                    "Masaż relaksacyjny to bardzo przyjemny zabieg, który pozwala osiągnąć pełną regenerację i odprężenie. Napełnia Cię nową energią i świeżymi pomysłami, dając poczucie lotu i joie de vivre.<br><br>Masaż relaksacyjny całego ciała powoduje rozluźnienie mięśni i przywrócenie witalności w całym organizmie.<br><br>Zabieg ten jest korzystny zarówno jako metoda lecznicza, jak i profilaktyczna. Dla osób zdrowych masaż relaksacyjny jest doskonałym zabiegiem antystresowym, który pozwala rozładować stres po całym dniu pracy.<br><br>Mechaniczne działanie na zakończenia nerwów obwodowych pobudza układ nerwowy, powodując produkcję przez organizm endorfin - hormonów szczęścia."
+                ], 
+                [
+                    "Marzyłaś o idealnie gładkiej skórze bez 'pomarańczowej skórki'?<br><br>Na początek masaż antycellulitowy może poprawić ogólny stan skóry, a także korzystnie wpłynąć na podskórną warstwę tłuszczu. Po kilku zabiegach następuje pobudzenie metabolizmu i zwiększenie krążenia krwi. Ostatecznie pomaga to szybciej schudnąć.<br><br>Masaż antycellulitowy stosowany jest na następujących obszarach: uda, pośladki i brzuch. Po masażu modelującym można osiągnąć takie rezultaty:<br><br>1. zwiększyć przepływ limfy i krążenie krwi;<br>2. normalizacja metabolizmu;<br>3. zmniejszenie widoczności cellulitu;<br>4. kształtowanie sylwetki;<br>5. zwiększenie elastyczności, jędrności i kolorytu skóry."
+                ], 
+                [
+                    "Masaż drenażowy limfatyczny to stosunkowo bezbolesny proces, który polega na głaskaniu i uciskaniu. Wszystkie manipulacje są delikatne i falujące, przeprowadzane powoli i bardzo delikatnie. Intensywność wzrasta w kierunku połowy sesji.<br><br>Aktywuje krążenie krwi, wspomaga krążenie limfy, poprawia metabolizm i spala tłuszcz. Wszystko to nie tylko pomaga w odchudzaniu, ale także ma ogólny dobry wpływ na organizm."
+                ], 
+                [
+                    "Każdy z nas chce mieć ujędrnione ciało. Aby uzyskać to, co chcesz, wiele osób idzie na siłownię i dostosować swoją dietę. W tym reżimie przydatny jest masaż sportowy.<br><br>Lekkie ruchy masujące rozluźniają i poprawiają funkcjonowanie układów wewnętrznych. Zabieg wykonuje się po treningu lub przed wysiłkiem. W pierwszym przypadku jest on potrzebny do regeneracji sił sportowca. W drugim przypadku pełni rolę rozgrzewki.<br><br>Masowanie zwiększa przepływ krwi i reguluje pracę gruczołów łojowych. Proces ten optymalizuje funkcjonowanie naczyń krwionośnych skóry, wzmacnia je i poprawia ich napięcie. Ponadto masaż pomaga złagodzić zmęczenie mięśni i rozwinąć stawy."
+                ]
+            ],
             fon: ["./media/classic.jpeg", "./media/relax.jpeg", "./media/anticelulit.jpeg", "./media/limf.jpeg", "./media/sport.jpeg"]
         },
         form: {
@@ -47,6 +95,7 @@ let openBlock = false;
 let upDown = true;
 let numberWindow = 0;
 const titleText = document.getElementById("titleText");
+const descriptionText = document.getElementById("descriptionText");
 const imgBackground = document.getElementById("main");
 const navBar = document.getElementsByClassName("navBar");
 const langIcon = document.getElementsByClassName("langSmallBlock");
@@ -83,6 +132,7 @@ function setupRuFunc() {
         navBar[i].innerText = multiLanguage.RU.header[i];
     }
     titleText.innerText = multiLanguage.RU.main.title[numberWindow];
+    descriptionText.innerHTML = multiLanguage.RU.main.description[numberWindow];
     langIcon[1].style.display = "none";
     langIcon[2].style.display = "none";
     btnOpenForm.innerText = multiLanguage.RU.button;
@@ -97,6 +147,7 @@ function setupEnFunc() {
         navBar[i].innerText = multiLanguage.EN.header[i];
     }
     titleText.innerText = multiLanguage.EN.main.title[numberWindow];
+    descriptionText.innerHTML = multiLanguage.EN.main.description[numberWindow];
     langIcon[0].style.display = "none";
     langIcon[2].style.display = "none";
     btnOpenForm.innerText = multiLanguage.EN.button;
@@ -111,6 +162,7 @@ function setupPlFunc() {
         navBar[i].innerText = multiLanguage.PL.header[i];
     }
     titleText.innerText = multiLanguage.PL.main.title[numberWindow];
+    descriptionText.innerHTML = multiLanguage.PL.main.description[numberWindow];
     langIcon[0].style.display = "none";
     langIcon[1].style.display = "none";
     btnOpenForm.innerText = multiLanguage.PL.button;
@@ -127,7 +179,7 @@ document.getElementById("btnOpenForm").onclick = function() {
         document.getElementById("formBlock").style.display = "block";
         openBlock = true;
         document.getElementById("btnOpenForm").disable = true;
-        document.getElementById("shadowMain").style.boxShadow = "0 0 0 9999px rgba(0,0,0, 0.3)";
+        document.getElementById("shadowMain").style.boxShadow = "0 0 0 9999px rgba(0,0,0, 0.6)";
     }
 }
 document.getElementById("closeFormBlock").onclick = function() {
@@ -169,14 +221,17 @@ function updateWindowSite(scrollingDirection) {
             case "RU":
                 titleText.innerText = multiLanguage.RU.main.title[numberWindow];
                 imgBackground.style.backgroundImage = `url(${multiLanguage.RU.main.fon[numberWindow]})`;
+                descriptionText.innerHTML = multiLanguage.RU.main.description[numberWindow];
                 break;
             case "EN":
                 titleText.innerText = multiLanguage.EN.main.title[numberWindow];
                 imgBackground.style.backgroundImage = `url(${multiLanguage.EN.main.fon[numberWindow]})`;
+                descriptionText.innerHTML = multiLanguage.EN.main.description[numberWindow];
                 break;
             case "PL":
                 titleText.innerText = multiLanguage.PL.main.title[numberWindow];
                 imgBackground.style.backgroundImage = `url(${multiLanguage.PL.main.fon[numberWindow]})`;
+                descriptionText.innerHTML = multiLanguage.PL.main.description[numberWindow];
                 break;
         }
     } else {
@@ -188,14 +243,17 @@ function updateWindowSite(scrollingDirection) {
             case "RU":
                 titleText.innerText = multiLanguage.RU.main.title[numberWindow];
                 imgBackground.style.backgroundImage = `url(${multiLanguage.RU.main.fon[numberWindow]})`;
+                descriptionText.innerHTML = multiLanguage.RU.main.description[numberWindow];
                 break;
             case "EN":
                 titleText.innerText = multiLanguage.EN.main.title[numberWindow];
                 imgBackground.style.backgroundImage = `url(${multiLanguage.EN.main.fon[numberWindow]})`;
+                descriptionText.innerHTML = multiLanguage.EN.main.description[numberWindow];
                 break;
             case "PL":
                 titleText.innerText = multiLanguage.PL.main.title[numberWindow];
                 imgBackground.style.backgroundImage = `url(${multiLanguage.PL.main.fon[numberWindow]})`;
+                descriptionText.innerHTML = multiLanguage.PL.main.description[numberWindow];
                 break;
         }
     }
